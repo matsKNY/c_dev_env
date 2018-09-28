b0f788db0c83532042615bba3f3102379c480010
Mathieu <mathieu.stoffel@atos.net>
2018-09-28 21:06:53 +0200

    [additional] youtube-dl + remarkable markdown

--- additional.sh
+++ additional.sh
@@ -11,3 +11,15 @@ apt-get update -y && apt upgrade -y && \
     apt install -y htop && \
     apt install -y pdftk && \
     apt install -y texlive-full texlive-xetex
+
+# Youtube-DL:
+apt install -y ffmpeg libsox-fmt-mp3
+curl -L https://yt-dl.org/downloads/latest/youtube-dl \
+    -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+
+# Remarkable Markdown:
+curl -L https://remarkableapp.github.io/files/remarkable_1.87_all.deb \
+    -o remarkable_markdown.deb 
+apt install ./remarkable_markdown.deb
+rm -fr remarkable_markdown.deb
